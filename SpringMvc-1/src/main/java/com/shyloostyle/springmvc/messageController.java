f64f4e57c6773192c71d5f26fc5d51eb4c7eadb5
Shrishail <122142836+shrishailmadari@users.noreply.github.com>
2024-12-18 22:55:43 +0530

    Add these files

---
+++ SpringMvc-1/src/main/java/com/shyloostyle/springmvc/messageController.java
@@ -0,0 +1,32 @@
+package com.shyloostyle.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+// to represent this class as controller
+
+@Controller
+public class messageController {
+    @GetMapping("/welcome")
+    public ModelAndView getWelcome(){
+
+        ModelAndView modelAndView = new ModelAndView();
+        //setting the response data in key value format
+        modelAndView.addObject("msg1", "WellCome Shrishail For First WebMvc");
+        modelAndView.addObject("msg2","heloooo");
+        //this is used to view the page: in which page it has to view
+        //that is in Index.html page it has to be viewed
+        modelAndView.setViewName("Index");
+//        the file name should be matching with setView Name
+        return modelAndView;
+
+    }
+    @GetMapping("/greet")
+    public ModelAndView getGreetMessage(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("value","Good Morning");
+        modelAndView.setViewName("Index");
+        return modelAndView;
+    }
+}
